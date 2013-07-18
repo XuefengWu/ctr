ec2ec954834deaf316af06909d7a0411db71722a
Xuefeng.Wu <benewu@gmail.com>
2013-07-18 22:52:28 +0800

    add assert for part of json node

--- test/app/RestTest.scala
+++ test/app/RestTest.scala
@@ -1,34 +1,35 @@
 package app
 
-
 import org.specs2.mutable.Specification
 import play.api.libs.ws.WS
 import play.api.test.Helpers._
 import scala.Some
-import play.api.libs.json.{JsValue, Json}
-import play.api.http.{Writeable, ContentTypeOf}
+import play.api.libs.json.{JsUndefined, JsNull, Json}
+import play.api.http.ContentTypeOf
+import play.api.libs
 import utils.{RestCase, RestCases}
 
 
-trait RestTest extends Specification  {
+trait RestTest extends Specification {
 
-  def testRests(file:String) = {
-    RestCases.parse(file).map{ c =>
-      testRestApi(c)
+  def testRests(file: String) = {
+    RestCases.parse(file).map {
+      c =>
+        testRestApi(c)
     }
   }
 
-  def testRestApi(c:RestCase) ={
+  def testRestApi(c: RestCase) = {
 
-    val (message,method,status,api,input,output) = (c.message,c.method,c.status,c.api,c.input,c.output)
+    val (message, method, status, api, input, output) = (c.message, c.method, c.status, c.api, c.input, c.output.trim)
 
     implicit val ctf = ContentTypeOf.contentTypeOf_JsValue
     api should {
       message in {
         println(s"$api-$message:$input")
         val response = method match {
-          case "get" => WS.url("http://localhost:9000"+api).get
-          case "post" => WS.url("http://localhost:9000"+api).post(Json.parse(input))
+          case "get" => WS.url("http://localhost:9000" + api).get
+          case "post" => WS.url("http://localhost:9000" + api).post(Json.parse(input))
         }
         val result = await(response)
         result.status === status
@@ -36,12 +37,20 @@ trait RestTest extends Specification  {
           case Some(ct) if ct.contains("text/html") => result.body must contain(output)
           case Some(ct) if ct.contains("text/plain") => result.body must contain(output)
           case Some(ct) if ct.contains("application/json") => {
-            if(!output.isEmpty){
+            if (!output.isEmpty) {
               val text = result.body
-              if(text.trim == output.trim){
+              if (text.trim == output) {
                 text === output
-              }else {
-                result.json === Json.parse(output)
+              } else {
+                if(output.startsWith("{")){
+                  result.json === Json.parse(output)
+                } else {
+                  output.split(",").map{ n =>
+                    val node = n.trim
+                    val json = result.json
+                    (json \ node).isInstanceOf[JsUndefined] must beFalse
+                  }.head
+                }
               }
             } else {
               "" === ""
@@ -56,4 +65,3 @@ trait RestTest extends Specification  {
   }
 
 }
-
